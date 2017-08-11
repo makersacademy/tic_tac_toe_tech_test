7f110ccbb5e356007ad796b97ccf0dbec7e93812
jinimcoroneo <jinim1983@gmail.com>
2017-08-11 21:06:24 +0100

    remove turn logic from board class

--- lib/board.rb
+++ lib/board.rb
@@ -4,19 +4,16 @@ class Board
                      4 => [1,0], 5 => [1,1], 6 => [1,2],
                      7 => [2,0], 8 => [2,1], 9 => [2,2]}
 
-  attr_reader :board, :player_1, :player_2, :turn
+  attr_reader :board, :player_1, :player_2, :player_turn
 
-  def initialize(player_1, player_2)
+  def initialize
     @board = board
-    @player_1 = player_1
-    @player_2 = player_2
-    @turn = 'X'
   end
 
   def create_board
-    @board = [[1,2,3],
-              [4,5,6],
-              [7,8,9]]
+    @board = [["1", "2", "3"],
+              ["4", "5", "6"],
+              ["7", "8", "9"]]
   end
 
   def print_board
@@ -24,33 +21,19 @@ class Board
     puts board
   end
 
-  def play_square(number)
-    find_location(number)
-    change_turn
-    print_board
-  end
-
-  def check_turn
-    @turn
+  def play(number, value)
+    find_location(number, value)
   end
 
   private
 
-  def change_turn
-    @turn == 'X' ? @turn = 'O' : @turn = 'X'
-  end
-
-  def find_location(number)
+  def find_location(number, value)
     location = SQUARE_LOCATION[number]
-    replace_square(location)
+    replace_square(location, value)
   end
 
-  def replace_square(location)
-    if check_turn == 'X'
-      @board[location[0]][location[1]] = 'X'
-    else
-      @board[location[0]][location[1]] = 'O'
-    end
+  def replace_square(location, value)
+    @board[location[0]][location[1]] = value
   end
 
 
